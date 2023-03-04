1e028af23e90d2cd0955d978be186f8aeb33b884
dazai <mydataisnotforfbi@proton.me>
2023-03-04 21:33:15 +0100

    Library

---
+++ RandomWordsGenerator/RandomWordsGenerator/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomWordsGenerator
+{
+    class Library
+    {
+        public static List<string> dwords = new List<string>()
+        {
+            " dupa", " kerbal", " nani", " DIO!", " kokoko", " NIGERUNDAIO",
+            " no kiss before marige", " c#", " calc", " .__."
+        };
+
+        private static int llen = dwords.Count;
+
+        public static int LLen
+        {
+            get { return llen; }
+            set { llen = value; }
+        }
+    }
+}
--- RandomWordsGenerator/RandomWordsGenerator/Program.cs
+++ RandomWordsGenerator/RandomWordsGenerator/Program.cs
@@ -19,40 +19,21 @@ namespace RandomWordsGenerator
                 for (int i = 0; i < wers_len; i++)
                 {
 
-                    int los_ = los(random);
-                    Console.Write(words(los_));
+                    int los_ = los(random, Library.LLen);
+                    Console.Write(word(los_, Library.dwords));
                 }
 
                 Console.WriteLine(); ;
             }
         }
 
-
-        static int los(Random random)
+        static int los(Random random, int ListLenght)
         {
-
-            int los = random.Next(1, 11);
+            int los = random.Next(0, ListLenght); 
             return los;
         }
-
-
-        static string words(int los)
+        static string word(int los, List<string> dwords)
         {
-
-            Dictionary<int, string> dwords = new Dictionary<int, string>();
-
-            // words collection
-            dwords.Add(1, " dupa");
-            dwords.Add(2, " kerbal");
-            dwords.Add(3, " nani");
-            dwords.Add(4, " DIO!");
-            dwords.Add(5, " kokoko");
-            dwords.Add(6, " NIGERUNDAIO");
-            dwords.Add(7, " no kiss before marige");
-            dwords.Add(8, " c#");
-            dwords.Add(9, " calc");
-            dwords.Add(10, " .______.");
-
             return dwords[los];
         }
     }
