b490ce66678886bb1b6013ecb83f054f09348bbb
dazai <mydataisnotforfbi@proton.me>
2023-03-05 22:07:33 +0100

    Console_notebook

---
+++ notebook_console/notebook_console/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace notebook_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+
+            Console.Clear();
+            var start = DateTime.Now;
+
+            string path = @"Notebook.txt"; // @ - sciesszka do pliku
+            StreamWriter streamWriter;
+
+            if (!File.Exists(path))
+            {
+                streamWriter = File.CreateText(path); // tworzymy plik
+                Console.WriteLine("\n File have created...");
+            }
+            else
+            {
+                streamWriter = new StreamWriter(path, true); // append = true - dołacz, faalse != append = zapisuje na nowo
+                Console.WriteLine("\n File have opened...");
+            }
+
+            Console.Write(start + "> "); var note = Console.ReadLine();
+
+            streamWriter.WriteLine(start + " | " + note); // zapisujemy do pliku
+            streamWriter.Close(); // zamykamy plik
+
+            goto start;
+        }
+    }
+}
