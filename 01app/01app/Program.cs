beb2894a10873d15c4d6906387497394c659ecf2
dazai <mydataisnotforfbi@proton.me>
2023-03-24 21:52:36 +0100

    lol

---
+++ 01app/01app/Program.cs
@@ -0,0 +1,31 @@
+﻿Random random = new Random();
+
+int los_numer()
+{
+
+    int los = 1;
+
+    los = random.Next(0, 10);
+    return los;
+}
+
+void get_los()
+{
+
+    while (true)
+    {
+
+        switch (Console.ReadKey(true).Key)
+        {
+
+            case ConsoleKey.L:
+                Console.WriteLine(los_numer());
+                break;
+        }
+    }
+}
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Thread reader = new(get_los);
+reader.Start();
